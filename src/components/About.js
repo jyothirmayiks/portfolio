e8e73330639643a571194b9b1496fa9cc71bd81e
jyothirmayiks <jothirmayiks@gmail.com>
2025-01-22 10:10:05 +0530

    feat: add about page

---
+++ src/components/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./About.css";
+import download from "/home/jyothirmayiks/portfolio_web/src/assets/download.png";
+
+const About = () => {
+  return (
+    <section id="about" className="about-section">
+      <div className="about-sidebar">
+        <h3>About Me</h3>
+      </div>
+      <div className="about-container">
+        <div className="about-text">
+          <h2>Who I am</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <p>
+            It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged.
+          </p>
+        </div>
+        <div className="about-image">
+          <img src={download} alt="jyothi" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
